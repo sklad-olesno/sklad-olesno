5fb93c1270849d2a5ca9e960261db1e05222bfc3
Patryk Padus <patryk.padus@smartmage.pl>
2019-02-20 22:16:25 +0100

    Aktualizacja

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,7 @@
     </div>
 
     <div class="mw7 center">
-      
+
       <ul class="flex flex-wrap justify-center ph2">
         <li class="w-33-ns ph2 mb4" v-for="section in page.sections">
           {{ section.section_title }}
@@ -40,19 +40,7 @@
       <div class="bg-off-white pv4">
         <div class="ph3 mw7 center">
           <h2 class="f2 b lh-title mb2">What we offer</h2>
-          <p class="mb4 mw6">Kaldi is the ultimate spot for coffee lovers who want to learn about their java’s origin and support the farmers that grew it. We take coffee production, roasting and brewing seriously and we’re glad to pass that knowledge to anyone.</p>
-
-          <div class="flex-ns mhn2-ns mb3">
-            <div class="ph2-ns w-50-ns">
-              <img src="img/illustrations/coffee.svg" alt="" class="center db mb3" style="width: 240px">
-              <p>We sell green and roasted coffee beans that are sourced directly from independent farmers and farm cooperatives. We’re proud to offer a variety of coffee beans grown with great care for the environment and local communities. Check our post or contact us directly for current availability.</p>
-            </div>
-
-            <div class="ph2-ns w-50-ns">
-              <img src="/img/illustrations/coffee-gear.svg" alt="" class="center db mb3" style="width: 240px">
-              <p>We offer a small, but carefully curated selection of brewing gear and tools for every taste and experience level. No matter if you roast your own beans or just bought your first french press, you’ll find a gadget to fall in love with in our shop.</p>
-            </div>
-          </div>
+          <p class="mb4 center">Kaldi is the ultimate spot for coffee lovers who want to learn about their java’s origin and support the farmers that grew it. We take coffee production, roasting and brewing seriously and we’re glad to pass that knowledge to anyone.</p>
 
 
           <div class="tc">
@@ -74,7 +62,7 @@
             </div>
 
             <div class="w-60-l ph2-l">
-              <img src="img/home/about-section.jpg" alt="" class="mb3">
+              <img :src="page.baner_image" alt="" class="mb3">
             </div>
           </div>
 

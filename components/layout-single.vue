dc3b5af6352093ab29cb05716b887f0dedf08bb0
Patryk Padus <patryk.padus@smartmage.pl>
2019-02-21 22:25:05 +0100

    Ciemniejsze nagłówki

--- components/layout-single.vue
+++ components/layout-single.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="mainClass" class="mw7 pv4 ph3 center">
 
-      <h2 class="f2 b lh-title mb2 tc" :class="data.dark ? 'grey-4' : 'primary'">
+      <h2 class="f2 b lh-title mb2 tc" :class="theme !== 'dark' ? 'grey-4' : 'primary'">
         {{ data.section_title }}
       </h2>
 

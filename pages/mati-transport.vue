508f44800ae7bb74780c9779171f2d03c8d19cc0
Patryk Padus <patryk.padus@smartmage.pl>
2019-02-27 21:15:10 +0100

    Aktualizacja

--- pages/mati-transport.vue
+++ pages/mati-transport.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mati-transport-block">
 
-    <h1 class="f1 primary tc ttu fw6 mt5 mb3 lh-solid">Mati transport</h1>
+    <h1 class="f1 primary tl ttu fw6 mt5 mb3 lh-solid">Mati transport</h1>
 
     <nuxtdown-body class="body" :body="page.body"/>
 

b2aa2c3443d67d09bad0ff4dc516a553e0e8b507
Patryk Padus <patryk.padus@smartmage.pl>
2019-02-28 23:58:36 +0100

    Klaudiusz - zmiana

--- pages/contact.vue
+++ pages/contact.vue
@@ -13,8 +13,7 @@
           <div class="ph1-ns w-50-ns">
             <h4 class="f4 b lh-title mb2 primary">Lokalizacja</h4>
             <address class="mb4">
-              {{ address.company_street_line_1 }}<br v-if="address.company_street_line_2 !== ''"/>
-              {{ address.company_street_line_2 }}<br/>
+              <span v-html="address.company_street"></span>
               {{ address.company_zip_code }} {{ address.company_city }}
             </address>
           </div>
@@ -120,13 +119,7 @@ export default {
     legalAgreement: false
   }),
   mounted(){
-    var street = this.address.company_street.split(/\n/);
-    this.address.company_street_line_1 = street[0];
-    this.address.company_street_line_2 = '';
-
-    if(street.length > 1){
-      this.address.company_street_line_2 = street[1];
-    }
+    this.address.company_street = this.address.company_street.split(/\n/).join('<br/>');
   },
   asyncData: async ({ app, route, payload }) => {
     return {

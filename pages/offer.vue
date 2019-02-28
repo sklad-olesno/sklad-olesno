c4436adce411d57c8dfc97e81263663e1b9c2284
Patryk Padus <patryk.padus@smartmage.pl>
2019-02-28 22:54:56 +0100

    Klaudiusz - zmiana

--- pages/offer.vue
+++ pages/offer.vue
@@ -4,6 +4,38 @@
     <h1 class="f1 primary tc ttu fw6 mt5 mb3 lh-solid">Oferta</h1>
 
     <nuxtdown-body class="body cms" :body="page.body"/>
+
+
+    <ul class="prices-block--list flex flex-wrap justify-center ph2">
+      <li class="w-33-ns ph2 mb4" v-for="item in page.products">
+
+        <h3 class="b f4 primary tl lh-title mb3">{{ item.name }}</h3>
+
+        <p class="b">
+          <img class="img-fluid" :src="item.image"/>
+        </p>
+
+        <ul class="tl">
+          <li>
+            <p class="pb2 mb2 divider-grey">Granulacja: {{ item.granulation }}</p>
+          </li>
+
+          <li>
+            <p class="pb2 mb2 divider-grey">Kaloryczność: {{ item.calorific }}</p>
+          </li>
+
+          <li>
+            <p class="">Popiół: {{ item.ash }}</p>
+          </li>
+
+        </ul>
+
+      </li>
+      <li class="w-33-ns ph2"></li>
+      <li class="w-33-ns ph2"></li>
+      <li class="w-33-ns ph2"></li>
+    </ul>
+
   </div>
 </template>
 

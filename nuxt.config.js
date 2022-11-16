285142bf11cfd2dfca8d1b70145947dcf80ac427
Triloworld <2671726+Triloworld@users.noreply.github.com>
2022-11-16 11:52:11 +0100

    Update price. Update dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,90 +1,84 @@
 module.exports = {
-  target: 'static',
+  target: "static",
   modules: [
     "nuxtdown",
     [
-      'nuxt-netlify-http2-server-push',
+      "@nuxtjs/pwa",
       {
-        // Specify relative path to the dist directory and its content type
-        resources: [
-          { path: '**/*.css', as: 'style' },
-          { path: '/_nuxt/content/pages/*.json', as: 'application/json' },
-        ]
-      }
+        icon: false,
+      },
     ],
     [
-      '@nuxtjs/pwa',
+      "@nuxtjs/google-tag-manager",
       {
-        icon: false
-      }
+        id: "GTM-N59H54M",
+      },
     ],
     [
-      '@nuxtjs/google-tag-manager',
+      "@nuxtjs/robots",
       {
-        id: 'GTM-N59H54M'
-      }
+        UserAgent: "*",
+        Disallow: "/admin",
+        Sitemap:
+          (process.env.BASE_URL || "http://localhost:3000") + "/sitemap.xml",
+      },
     ],
     [
-      '@nuxtjs/robots',
+      "@reallifedigital/nuxt-image-loader-module",
       {
-        UserAgent: '*',
-        Disallow: '/admin',
-        Sitemap: (process.env.BASE_URL || 'http://localhost:3000') + '/sitemap.xml'
-      }
-    ],
-    ['@reallifedigital/nuxt-image-loader-module', {
-      imagesBaseDir: 'dist',
-      imageStyles: {
-        small: {
-          macros: ['scaleAndCrop|160|90'],
-          actions: ['quality|80']
+        imagesBaseDir: "dist",
+        imageStyles: {
+          small: {
+            macros: ["scaleAndCrop|160|90"],
+            actions: ["quality|80"],
+          },
+          medium: {
+            macros: ["scaleAndCrop|320|180"],
+            actions: ["quality|80"],
+          },
+          large: {
+            macros: ["scaleAndCrop|640|360"],
+            actions: ["quality|80"],
+          },
+          banner: {
+            macros: ["scaleAndCrop|1920|1080"],
+            actions: ["quality|90"],
+          },
         },
-        medium: {
-          macros: ['scaleAndCrop|320|180'],
-          actions: ['quality|80']
-        },
-        large: {
-          macros: ['scaleAndCrop|640|360'],
-          actions: ['quality|80']
-        },
-        banner: {
-          macros: ['scaleAndCrop|1920|1080'],
-          actions: ['quality|90']
+        // Optional responsive style profiles:
+        responsiveStyles: {
+          large: {
+            srcset: "small 160w, medium 320w, large 640w, banner 1200w",
+            sizes: "(min-width: 1280px) 100vw, 50vw",
+          },
+          thumb: {
+            srcset: "small 160w, medium 320w, large 640w",
+            sizes: "(min-width: 1280px) 100vw, 50vw",
+          },
         },
       },
-      // Optional responsive style profiles:
-      responsiveStyles: {
-        large: {
-          srcset: 'small 160w, medium 320w, large 640w, banner 1200w',
-          sizes: '(min-width: 1280px) 100vw, 50vw',
-        },
-        thumb: {
-          srcset: 'small 160w, medium 320w, large 640w',
-          sizes: '(min-width: 1280px) 100vw, 50vw',
-        },
-      },
-    }],
-    '@nuxtjs/markdownit',
-    '@nuxtjs/sitemap',
-    'nuxt-polyfill'
+    ],
+    "@nuxtjs/markdownit",
+    "@nuxtjs/sitemap",
+    "nuxt-polyfill",
   ],
   manifest: {
-    lang: 'pl',
+    lang: "pl",
     display: "browser",
   },
   markdownit: {
-    preset: 'default',
+    preset: "default",
     html: true,
     typographer: true,
     linkify: true,
     breaks: true,
-    injected: true
+    injected: true,
   },
   env: {
-    BASE_URL: process.env.BASE_URL || 'http://localhost:3000'
+    BASE_URL: process.env.BASE_URL || "http://localhost:3000",
   },
   sitemap: {
-    hostname: process.env.BASE_URL || 'http://localhost:3000'
+    hostname: process.env.BASE_URL || "http://localhost:3000",
   },
   polyfill: {
     features: [
@@ -92,46 +86,39 @@ module.exports = {
       //   require: 'intersection-observer',
       //   detect: () => 'IntersectionObserver' in window,
       // }
-    ]
+    ],
   },
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     htmlAttrs: {
-      lang: 'pl',
+      lang: "pl",
     },
     title: "Skład opału - Olesno",
-    meta:
-      [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
-        {
-          hid: "description",
-          name: "description",
-          content: "Skład opału wraz z przewozem na terenie Olesna granic"
-        },
-        {name: "robots", content: "INDEX, FOLLOW"}
-      ],
-    link:
-      [
-        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
-      ],
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Skład opału wraz z przewozem na terenie Olesna granic",
+      },
+      { name: "robots", content: "INDEX, FOLLOW" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   generate: {
     dir: "public",
   },
   /*
-  ** Customize the progress bar color
-  */
+   ** Customize the progress bar color
+   */
   loading: { color: "#FFBB43" },
-  css: [
-    '~/assets/styles.css',
-    '~/assets/custom.css'
-  ],
+  css: ["~/assets/styles.css", "~/assets/custom.css"],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     html: {
       minify: {
@@ -146,21 +133,21 @@ module.exports = {
         removeEmptyAttributes: true,
         removeRedundantAttributes: true,
         trimCustomFragments: true,
-        useShortDoctype: true
-      }
+        useShortDoctype: true,
+      },
     },
     /*
-    ** Run ESLint on save
-    */
+     ** Run ESLint on save
+     */
     extend(config, { isDev }) {
       if (isDev && process.client) {
         config.module.rules.push({
           enforce: "pre",
           test: /\.(js|vue)$/,
           loader: "eslint-loader",
-          exclude: /(node_modules)/
+          exclude: /(node_modules)/,
         });
       }
-    }
+    },
   },
 };
--- pages/prices.vue
+++ pages/prices.vue
@@ -1,60 +1,85 @@
 <template>
   <div class="prices-block">
-
     <h1 class="f1 primary tc ttu fw6 mt5 mb4 lh-solid">Cennik</h1>
 
     <ul class="prices-block--list flex flex-wrap justify-center ph2">
       <li class="w-33-ns ph2 mb4" v-for="item in page.prices">
-
-          <h3 class="b f4 primary tl lh-title mb3">{{ item.name }}</h3>
-          <div class="flex">
-            <p class="secondary f3 b tl lh-title center mb1 flex flex-column w-75 flex-auto">
-              {{ item.non_packed }}
-              <span class="f6">luzem</span>
-            </p>
-            <p class="grey-3 f3 b tl lh-title center flex flex-column w-100 flex-auto">
-              {{ item.packed }}
-              <span class="f6">worek</span>
-            </p>
-          </div>
-
-          <p class="b">
-            <nuxt-img :src="item.image" responsive-style="thumb" :alt="item.name" class="img-fluid"/>
+        <h3 class="b f4 primary tl lh-title mb3">{{ item.name }}</h3>
+        <div class="flex">
+          <p
+            class="
+              secondary
+              f6
+              b
+              tl
+              lh-title
+              center
+              mb1
+              flex flex-column
+              w-100
+              flex-auto
+            "
+          >
+            (skontaktuj się)
+            <span class="f6">luzem</span>
           </p>
+          <p
+            class="
+              grey-3
+              f6
+              b
+              tl
+              lh-title
+              center
+              flex flex-column
+              w-100
+              flex-auto
+            "
+          >
+            (skontaktuj się)
+            <span class="f6">worek</span>
+          </p>
+        </div>
 
+        <p class="b">
+          <nuxt-img
+            :src="item.image"
+            responsive-style="thumb"
+            :alt="item.name"
+            class="img-fluid"
+          />
+        </p>
       </li>
       <li class="w-33-ns ph2"></li>
       <li class="w-33-ns ph2"></li>
       <li class="w-33-ns ph2"></li>
     </ul>
-
   </div>
 </template>
 
 <script>
 export default {
-  head: function() {
+  head: function () {
     return {
       title: `${this.page.title}`,
       meta: [
         {
           hid: "description",
           name: "description",
-          content: this.page.description
-        }
-      ]
+          content: this.page.description,
+        },
+      ],
     };
   },
   mounted() {
-    this.$el.classList.remove('pb5');
+    this.$el.classList.remove("pb5");
   },
   asyncData: async ({ app, route, payload }) => {
     return {
-      page: (await app.$content("/pages").get(route.path)) || payload
+      page: (await app.$content("/pages").get(route.path)) || payload,
     };
-  }
+  },
 };
 </script>
 
-<style type="text/css">
-</style>
+<style type="text/css"></style>
